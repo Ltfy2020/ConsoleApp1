10553f559036b776cd8aaee6cf592480adb8dde4
My_PC <My_PC@DESKTOP-JGJB9EV>
2018-09-08 18:52:42 +0300

    add class1

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
+        int c;
+        string Str = "It's good";
 
-
-        }
     }
 }
